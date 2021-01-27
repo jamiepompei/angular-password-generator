f5625eb58e653cb70957db9ed38008a55f5c954f
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-27 10:34:11 -0500

    added event handlers for generate button and check boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  includeSymbols = false;
+  includeNumbers = false;
+  includeLetters = false;
+  password = '';
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onButtonClick() {
+    console.log(this.includeSymbols);
+    this.password = 'MY PASSWORD';
+  }
+
+
+
 }
